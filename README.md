07391d45a8aac6f1773e4be3955f8cc850dcf412
Renato Setoue <renatosetoue@gmail.com>
2023-08-26 23:49:29 -0300

    v0.2.0

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# sessao-da-tarde
+# Sessão da Tarde Generator
+
+Sessão da Tarde TV spot text generator.
+
+## Table of Contents
+
+- [About](#about)
+- [Live Demo](#live-demo)
+- [Built Using](#built-using)
+- [Release History](#history)
+- [Credits](#credits)
+- [Author](#author)
+- [License](#license)
+
+## About <a name = "about"></a>
+
+It popped up in a conversation and I looked for a Sessão da Tarde generator but I could only find one - I thought there were more, early 2000s were full of generators.
+
+But you had to choose all the options so I thought it would work better if I could simply randomize the choices. I made a few adjustments here and there in the text, added a few things and that's how it started. I'm making this repository because I intend to expand on that later.
+
+## Live Demo <a name = "live-demo"></a>
+
+Sessão da Tarde Generator ([demo](https://psudo-prototype.netlify.app/sessao-da-tarde-generator/))
+
+## Built Using <a name = "built-using"></a>
+
+- HTML
+- CSS
+- Javascript
+
+## Release History <a name = "history"></a>
+
+- 0.2.0
+  - cleaned up the code - no more repetition
+- 0.1.1
+  - max-width: 800px for the header image
+  - added more actors
+  - text changes
+- 0.1.0
+  - The first proper release
+- 0.0.1
+  - Work in progress
+
+## Credits <a name = "credits"></a>
+
+- [Chamada Generator](http://alexandrenix.com/sessao/)
+
+## Author <a name = "author"></a>
+
+[@psudo-dev](https://github.com/psudo-dev)
+
+## License <a name = "license"></a>
+
+This project is licensed under the MIT License - see the [LICENSE.md](./LICENSE.md) file for details
---
+++ index.js
@@ -0,0 +1,62 @@
+// options to be randomized
+const options = {
+    diasDaSemana: ["Nesta Segunda-Feira", "Nesta Terça-Feira", "Nesta Quarta-Feira", "Nesta Quinta-Feira", "Nesta Sexta-Feira"],
+    atores: ["Steve Martin", "Chevy Chase", "Eddie Murphy", "Denny Devitto", "Bill Murray", "John Candy", "Tom Hanks", "Sylvester Stallone", "Arnold Schwarzenegger"],
+    papel: [
+        ["é o líder de uma turma", "essa galerinha"],
+        ["é o irmão mais velho de Rick Moranis,", "essa dupla"],
+        ["renasce como um cachorro", "esse cachorro"],
+        ["é um policial", "esse tira"]
+    ],
+    enredo: ["que é acusado injustamente por um crime que não cometeu", "que acaba de pintar na área", "que recebe um estranho presente", "que tem um talento muito especial"],
+    option1: ["Vai rolar", "Será", "Vai sobrar", "Vocês verão"],
+    option2: ["uma tremenda", "uma grande", "muita"],
+    fraseDeEfeito1: ["ninguém botar defeito", "todo mundo", "dar e vender", "todos os lados"],
+    fraseDeEfeito2: ["detonar tudo", "encarar esse desafio", "jogar tudo pro alto", "botar pra quebrar"],
+    adjetivo1: ["emocionante", "eletrizante", "incrível", "fantástica"],
+    option3: ["jornada", "missão", "fuga", "viagem"],
+    adjetivo2: ["aventura", "agito", "zoação", "diversão"],
+    adjetivo3: ["muitas enrascadas", "muita azaração", "muitas roubadas", "altas frias"]
+}
+
+// takes an object with the options and return another with the choices
+const randomizer = (optionsObject) => {
+    const optionsToArray = (Object.values(optionsObject));
+    const optionsArray = []
+    optionsToArray.forEach((option) => {
+        const index = Math.floor(Math.random() * (option.length))
+        const random = option[index]
+        if (Array.isArray(random)) {
+            optionsArray.push(random[0])
+            optionsArray.push(random[1])
+        } else {
+            optionsArray.push(random)
+        }
+    })
+    return { // returning an object because it's easier to understand
+        diaDaSemana: optionsArray[0],
+        ator: optionsArray[1],
+        papel1: optionsArray[2],
+        papel2: optionsArray[3],
+        enredo: optionsArray[4],
+        option1: optionsArray[5],
+        option2: optionsArray[6],
+        fraseDeEfeito1: optionsArray[7],
+        fraseDeEfeito2: optionsArray[8],
+        adjetivo1: optionsArray[9],
+        option3: optionsArray[10],
+        adjetivo2: optionsArray[11],
+        adjetivo3: optionsArray[12]
+    }
+}
+
+const chamada = (selectedOptions) => { // text contructor
+    const text = `Embarque em mais um grande sucesso do cinema: ${selectedOptions.ator} ${selectedOptions.papel1} ${selectedOptions.enredo}. ${selectedOptions.option1} ${selectedOptions.option2} confusão para ${selectedOptions.fraseDeEfeito1} quando ${selectedOptions.papel2} resolve ${selectedOptions.fraseDeEfeito2}. Uma ${selectedOptions.adjetivo1} ${selectedOptions.option3} em que não vai faltar ${selectedOptions.adjetivo2} e ${selectedOptions.adjetivo3}!`
+    document.querySelector(".card-subtitle").textContent = `${selectedOptions.diaDaSemana}`
+    document.querySelector(".card-text").textContent = text
+}
+
+chamada(randomizer(options))
+document.querySelector("button").addEventListener("click", () => {
+    chamada(randomizer(options))
+})
